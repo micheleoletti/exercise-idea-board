7996b91b83b2f2765f6f9b3812b2cd547344a486
Mykhaylo Olefirenko <molefirenko@maypdigital.com>
2022-07-26 14:48:19 +0000

    fix missing styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import AuthProvider from "../contexts/AuthContext";
 import { IdeaProvider } from "../contexts/IdeaContext";
-import "../styles/globals.css";
+// import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
