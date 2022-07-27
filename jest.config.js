10c86f020488442230ee42137b232b62c701e384
Mykhaylo Olefirenko <molefirenko@maypdigital.com>
2022-07-27 23:51:15 +0000

    fix unit test paths

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,26 @@
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
-    '^@/components/(.*)$': '<rootDir>/components/$1',
+    "^@/components/(.*)$": "<rootDir>/components/$1",
 
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
 
     // https://www.sobyte.net/post/2022-06/jest/, 5 hours lost right there
-    '^uuid$': require.resolve('uuid'),
-
+    "^uuid$": require.resolve("uuid"),
   },
-  testEnvironment: 'jest-environment-jsdom',
-}
+  testEnvironment: "jest-environment-jsdom",
+  // add this to enable absolute paths
+  modulePaths: ["<rootDir>"],
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
